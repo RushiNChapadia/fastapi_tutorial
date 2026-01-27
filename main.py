25724ff3d4cb3f0d0d036480b0886fe92ea04cc5
RushiNChapadia <rushichapadia05@gmail.com>
2026-01-27 11:37:24 -0800

    Created some endpoints

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
+
+def load_data():
+    with open('patients.json','r') as file:
+        data = json.load(file)
+    return data
 
 @app.get("/")
 def hello():
@@ -9,3 +15,17 @@ def hello():
 @app.get("/about")
 def about():
     return {"message": "This is a simple FastAPI application."}
+
+
+@app.get("/view")
+def view():
+    data = load_data()
+    return data
+
+@app.get("/patient/{patient_id}")
+def view_patient(patient_id: str):
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    return {'error': 'patient not found'}
